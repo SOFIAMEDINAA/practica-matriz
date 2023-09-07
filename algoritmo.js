9af943213d73806b08692b101e5f56b4c0393a1c
Sofia Medina <medinasofiaa1@gmail.com>
2023-09-07 11:52:55 -0300

    cambio de color y deteccion de casillas

--- algoritmo.js
+++ algoritmo.js
@@ -2,38 +2,56 @@ const divTablero = document.querySelector("#tablero");
 
 
 const matriz = []
+let cantCasillas = 0;
 
 function iniciar() {
     let inputFila = parseInt(document.querySelector("#fila").value);
-    divTablero.innerHTML =""
+    cantCasillas = 0;
+    divTablero.innerHTML = ""
 
     for (let fila = 0; fila < inputFila; fila++) {
         divTablero.innerHTML += `
     <div class="row">
-        ${agregarColumna()}
+        ${agregarColumna(fila)}
         </div>`
     }
 
 
-
 }
-function agregarColumna() {
+function agregarColumna(fila) {
 
 
-     let inputColumna = parseInt(document.querySelector("#columna").value);
+    let inputColumna = parseInt(document.querySelector("#columna").value);
 
     let columnas = "";
 
     for (let columna = 0; columna < inputColumna; columna++) {
 
-        columnas += `<div class="col casilla">
-        </div>`
-
+        if (cantCasillas % 2 == 0) {
+            columnas += `
+                <div id="${fila}-${columna}" onclick="detectar(this)" class="col casilla casilla-1">
+                </div>
+            `;
+        } else {
+            columnas += `
+                <div id="${fila}-${columna}" onclick="detectar(this)" class="col casilla casilla-0">
+                </div>
+            `;
+        }
+        cantCasillas++;
     }
+    cantCasillas++;
     return columnas; 
+   
 }
 
-function cambiarColores() {
-    
-
+function detectar(elemento) {
+    alert(elemento.id);
+    console.log(elemento.id);
+ 
 }
+
+
+/* casilla.addEventListener('click', () => {
+    console.log("Clickeaste la casilla en la posición" + matriz + fila + columna);
+} */
